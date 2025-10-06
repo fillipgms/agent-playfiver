3292c2af98ba978488156f8227ac4091bd946a22
Fillip Mangia <fillipgms@gmail.com>
2025-10-06 17:27:14 -0300

    customization page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,7 @@ import {
     UsersThreeIcon,
     ArrowsLeftRightIcon,
     FileTextIcon,
-    // PaletteIcon,
+    PaletteIcon,
     UserCircleIcon,
 } from "@phosphor-icons/react";
 import GridIcon from "@/public/GridIcon";
